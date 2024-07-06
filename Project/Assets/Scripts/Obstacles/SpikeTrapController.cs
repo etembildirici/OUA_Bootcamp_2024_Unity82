d7eb290a61c7e5de5cd1259951e7b2305ae03397
etembildirici <ethem.zblr@gmail.com>
2024-07-06 03:16:09 +0300

    Update Obstacles

---
+++ Project/Assets/Scripts/Obstacles/SpikeTrapController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeTrapController : MonoBehaviour {
+
+    public Animator spikeTrapAnim; // SpikeTrap için Animator
+    public float openCloseDelay = 2f; // Açılıp kapanma arasındaki varsayılan gecikme süresi
+    public float startDelay = 0f; // Başlangıç gecikmesi süresi
+
+    void Awake()
+    {
+        spikeTrapAnim = GetComponent<Animator>();
+
+        // Rastgele bir sayı oluştur
+        float randomValue = Random.value;
+
+        if (randomValue <= 0.2f) // %20 ihtimalle tamamen kapalı
+        {
+            CloseTrap();
+        }
+        else if (randomValue <= 0.6f) // %40 ihtimalle başlangıçta belirli bir süre gecikme
+        {
+            StartCoroutine(StartWithDelay());
+        }
+        else // %40 ihtimalle normal çalışma
+        {
+            StartCoroutine(OpenCloseTrap());
+        }
+    }
+
+    IEnumerator StartWithDelay()
+    {
+        yield return new WaitForSeconds(startDelay);
+        StartCoroutine(OpenCloseTrap());
+    }
+
+    IEnumerator OpenCloseTrap()
+    {
+        while (true)
+        {
+            OpenTrap();
+            yield return new WaitForSeconds(openCloseDelay);
+            CloseTrap();
+            yield return new WaitForSeconds(openCloseDelay);
+        }
+    }
+
+    void OpenTrap()
+    {
+        spikeTrapAnim.SetTrigger("open");
+    }
+
+    void CloseTrap()
+    {
+        spikeTrapAnim.SetTrigger("close");
+    }
+}
