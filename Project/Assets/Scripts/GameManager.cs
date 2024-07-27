10c223f72af22c6e3d84eccd132d070ad041fcf0
esraugrbs <115251056+esraugrbs@users.noreply.github.com>
2024-07-27 20:40:17 +0300

    .

---
+++ Project/Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+    public string SelectedCharacter;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void SelectCharacter(string characterName)
+    {
+        SelectedCharacter = characterName;
+    }
+}
