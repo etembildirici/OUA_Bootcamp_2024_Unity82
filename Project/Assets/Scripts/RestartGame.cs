7d21016e7b2a045e45c6659f7c4a181cfc3dee0b
etembildirici <ethem.zblr@gmail.com>
2024-07-29 18:10:34 +0300

    Update Level

--- Project/Assets/Scripts/RestartGame.cs
+++ Project/Assets/Scripts/RestartGame.cs
@@ -5,15 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class RestartGame : MonoBehaviour
 {
-    public GameManager gameManager; // GameManager referans?
+    public GameManager gameManager; // GameManager referansý
+    private bool isPlayerAlive = true;
 
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Spike") || other.CompareTag("Water"))
         {
+            isPlayerAlive = false;
             Debug.Log("Player collided with obstacle!");
-            gameManager.EndGame();// Oyunu durdur ve yeniden ba?lama butonunu g?ster
-
+            gameManager.EndGame(); // Oyunu durdur ve yeniden baþlama butonunu göster
         }
     }
 }
